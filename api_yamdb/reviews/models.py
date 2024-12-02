e0dbe0692e7cac05db7462a55a2ed8d447196ed2
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2024-12-02 20:12:48 +0300

    create base models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Title(models.Model):
+    title = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    category = models.OneToOneField(
+        Category, on_delete=models.SET_NULL,
+        related_name='titles', blank=True, null=True
+    )
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL,
+        related_name='titles', blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Reviews(models.Model):
+    # author = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, related_name='comments')
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    # author = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Reviews, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
